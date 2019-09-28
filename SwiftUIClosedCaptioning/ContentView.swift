964ed2aeccc221d8f51aa81b0b7e848535a0b8e0
Danny Bolella <dbolella@gmail.com>
2019-09-28 12:58:32 -0400

    Initial Commit

---
+++ SwiftUIClosedCaptioning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIClosedCaptioning
+//
+//  Created by Daniel Bolella on 9/28/19.
+//  Copyright © 2019 Daniel Bolella. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
